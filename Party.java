5c16071ad90b0a68b9d05ce54d6108fddd93d679
Siddhartha Gupta <siddhero97@gmail.com>
2020-10-18 18:55:47 +0530

    added code for assignemnt from Oct 17

---
+++ Party.java
@@ -0,0 +1,15 @@
+class Party{
+public static void main(String[] args) {
+	Party.music();
+	Party.drinking();
+	}
+	
+public static void music(){
+System.out.println("Playing good music");
+}
+
+public static void drinking() {
+System.out.println("Drinking ");
+}
+
+}
