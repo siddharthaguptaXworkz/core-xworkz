75c8155dac7e2d182de64b7468fa10cc998aa3b6
Siddhartha Gupta <siddhero97@gmail.com>
2020-10-22 23:40:57 +0530

    added festival, state and accessory

---
+++ AssOct22/Accessories.java
@@ -0,0 +1,13 @@
+public class Accessories{
+	public static void main(String[] args) {
+		String accessory0 = args[0];
+		String accessory1 = args[1];
+		String accessory2 = args[2];
+		String accessory3 = args[3];
+		System.out.println("accessory0 "+ accessory0);
+		System.out.println("accessory1 "+ accessory1);
+		System.out.println("accessory2 "+accessory2);
+		System.out.println("accessory3 "+accessory3);
+	}
+
+}
---
+++ AssOct22/Festival.java
@@ -0,0 +1,13 @@
+public class Festival{
+	public static void main(String[] args) {
+		String festival0 = args[0];
+		String festtival1 = args[1];
+		String festival2 = args[2];
+		String festival3 = args[3];
+		System.out.println("festival0 "+ festival0);
+		System.out.println("festtival1 "+ festtival1);
+		System.out.println("festival2 "+festival2);
+		System.out.println("festival3 "+festival3);
+	}
+
+}
