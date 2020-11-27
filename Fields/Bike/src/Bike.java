d7c8f227b577214a9046434f962cbcd0fcb8328c
Siddhartha Gupta <siddhero97@gmail.com>
2020-11-27 10:35:02 +0530

    add bike

---
+++ Fields/Bike/src/Bike.java
@@ -0,0 +1,12 @@
+public class Bike {
+    private String dest;
+
+    public Bike(String dest) {
+        this.dest = dest;
+    }
+
+    public void displayInfo() {
+        System.out.println("dest = " + dest);
+    }
+}
+
---
+++ Fields/Bike/src/BikeTester.java
@@ -0,0 +1,10 @@
+public class BikeTester {
+    public static void main(String[] args) {
+        Bike toBangalore = new Bike("Bangalore");
+        toBangalore.displayInfo();
+        Bike toMumbai = new Bike("Mumbai");
+        toMumbai.displayInfo();
+        Bike toCalgary = new Bike("Calgary");
+        toCalgary.displayInfo();
+    }
+}
